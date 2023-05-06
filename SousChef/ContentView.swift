f9a1691dd329f5aae0ffb05f3a5606180b3e8b30
alexdesab <alexdesab@gmail.com>
2023-05-05 22:16:52 -0400

    Initial Commit

---
+++ SousChef/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SousChef
+//
+//  Created by Alex De Sabatino on 5/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SousChef/SousChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  SousChefApp.swift
+//  SousChef
+//
+//  Created by Alex De Sabatino on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SousChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
